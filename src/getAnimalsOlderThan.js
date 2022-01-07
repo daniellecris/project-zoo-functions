b6e44b5b2444deeb7993ef3aeb7eb9c173227ccf
Danielle Cristina <dnaielle.trybe@gmail.com>
2022-01-07 17:14:11 -0300

    Requisitos 1 e 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,8 +1,15 @@
 const data = require('../data/zoo_data');
 
+// Esta função, a partir do nome de uma espécie e uma idade mínima,
+// verifica se todos os animais daquela espécie possuem a idade mínima especificada. retorna booleano
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-  //Iniciando Projeto
+  const specie = data.species.find((specieFind) => specieFind.name === animal);
+  // every, percorre todos os itens do array verificando se corresponde ao critério, retornando booelano
+  const animaisAge = specie.residents.every((specieAnimal) => specieAnimal.age > age);
+  return animaisAge;
 }
+
+getAnimalsOlderThan('otters', 7);
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (!ids) return 0;
+
+  const animaisId = ids.map((specie) => species.find((specieId) => specieId.id === specie));
+  return animaisId;
 }
 
 module.exports = getSpeciesByIds;
