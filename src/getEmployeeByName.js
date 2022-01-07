65e94d96b9c3f868bad5e99591aa5cdce17b870c
Danielle Cristina <dnaielle.trybe@gmail.com>
2022-01-07 18:55:31 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+
+  const funcionario = data.employees.find((func) =>
+    (func.lastName === employeeName) || (func.firstName === employeeName));
+  return (funcionario);
 }
 
 module.exports = getEmployeeByName;
