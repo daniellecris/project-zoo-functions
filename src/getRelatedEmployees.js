e07433ed21c0d0126b55b628ca7842bfd15b79cd
Danielle Cristina <dnaielle.trybe@gmail.com>
2022-01-08 18:39:10 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// verificar se o colaborador é gerente ou não, retornando booleano;
 function isManager(id) {
-  // seu código aqui
+  return employees.some((manager) => manager.managers.includes(id));
 }
 
+// retorna array com nome dos funcionários que o colaborador é gerente.
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return employees.filter((employee) => employee.managers
+      .includes(managerId))
+      .map((employeeName) => `${employeeName.firstName} ${employeeName.lastName}`);
+  }
+  // se não for gerente, gerar erro com a mensagem "O id inserido não é de uma pessoa colaboradora gerente!".
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
