72684b93a2d8bed664447e1bbb426b38072d277d
Danielle Cristina <dnaielle.trybe@gmail.com>
2022-01-10 20:02:54 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = (entrants.filter((entrant) => entrant.age < 18)).length;
+  const adult = (entrants.filter((entrant) => (entrant.age >= 18 && entrant.age < 50))).length;
+  const senior = (entrants.filter((entrant) => entrant.age >= 50)).length;
+
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+
+  const totalVisits = countEntrants(entrants);
+  const totalChild = totalVisits.child * prices.child;
+  const totalAdult = totalVisits.adult * prices.adult;
+  const totalSenior = totalVisits.senior * prices.senior;
+
+  return totalChild + totalSenior + totalAdult;
 }
 
 module.exports = { calculateEntry, countEntrants };
