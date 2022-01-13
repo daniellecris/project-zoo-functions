8bf160a7922e7a011c211976a44ad12680e9d5b4
Danielle Cristina <dnaielle.trybe@gmail.com>
2022-01-12 21:02:35 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const pesqEmployee = employees.find((employee) => id === employee.id);
+
+  // pesquisa a primeira especie que o funcionário é responsável
+  const pesqSpecie = species.find((animal) => animal.id === pesqEmployee.responsibleFor[0]);
+
+  const animal = pesqSpecie.residents.sort((a, b) => b.age - a.age);
+
+  return Object.values(animal[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
