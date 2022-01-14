624f9e674e87729bb3057827e3a5e595cb260fbc
Danielle Cristina <dnaielle.trybe@gmail.com>
2022-01-13 22:45:46 -0300

    Requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,36 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+const dataHour = Object.entries(data.hours);
+const dayWeek = Object.keys(data.hours);
+
+// Requisito com auxílio do Thalles - Monitoria Individual, Baêta conclusão sala de estudos (Léo e Brendha)
+function getParam(param) {
+  // se o parametro for vazio ou qualquer coisa.
+  return dataHour.reduce((acc, [day, status]) => {
+    acc[day] = {
+      officeHour: `Open from ${status.open}am until ${status.close}pm`,
+      exhibition: species.filter((specie) => specie.availability.includes(day))
+        .map((specie) => specie.name),
+    };
+    acc.Monday = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+    return acc;
+  }, {});
+}
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const animalPesq = species.find((specie) => specie.name === scheduleTarget);
+  if (animalPesq) {
+    return animalPesq.availability;
+  }
+
+  if (dayWeek.includes(scheduleTarget)) {
+    const obj = { [scheduleTarget]: getParam()[scheduleTarget] };
+    return obj;
+  }
+  return getParam();
 }
+// getSchedule();
+console.log(getSchedule('lions'));
 
 module.exports = getSchedule;
